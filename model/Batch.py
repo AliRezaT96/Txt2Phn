d818b902fb3dcfb660216b3dac47e7c2fedf3aa5
ArT <74541549+AliRezaT96@users.noreply.github.com>
2022-06-14 21:11:02 +0430

    Update Batch.py

--- model/Batch.py
+++ model/Batch.py
@@ -4,12 +4,9 @@ import numpy as np
 from torch.autograd import Variable
 
 
-def nopeak_mask(size, opt):
-    np_mask = np.triu(np.ones((1, size, size)),
-    k=1).astype('uint8')
-    np_mask =  Variable(torch.from_numpy(np_mask) == 0)
-    if opt.device == 0:
-      np_mask = np_mask.cuda()
+def nopeak_mask(size):
+    np_mask = np.triu(np.ones((1, size, size)),k=1).astype('uint8')
+    np_mask =  Variable(torch.from_numpy(np_mask) == 0
     return np_mask
 
 def create_masks(src, trg, opt):
@@ -19,9 +16,7 @@ def create_masks(src, trg, opt):
     if trg is not None:
         trg_mask = (trg != opt.trg_pad).unsqueeze(-2)
         size = trg.size(1) # get seq_len for matrix
-        np_mask = nopeak_mask(size, opt)
-        if trg.is_cuda:
-            np_mask.cuda()
+        np_mask = nopeak_mask(size)
         trg_mask = trg_mask & np_mask
         
     else:
